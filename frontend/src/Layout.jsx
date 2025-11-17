ccce1243812be255f80473bd02e3716ce8e7330f
Emmanuel <gocaem@gmail.com>
2025-11-17 13:55:41 -0800

    version added

---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import './Layout.css';
+
+const Layout = () => {
+  return (
+    <div className="layout-container">
+      <main className="main-content">
+        <Outlet />
+      </main>
+      <footer className="app-footer">
+        <p>Blackjack Game v1.0.0 | © 2024</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
